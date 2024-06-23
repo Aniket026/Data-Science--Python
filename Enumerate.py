4b8cc809a3c46f2607e0d424e0b531e196bff34b
Aniket Borawake <131742395+Aniket026@users.noreply.github.com>
2024-06-23 10:34:45 +0530

    Enumerate

---
+++ Enumerate.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 10:27:00 2024
+
+@author: aniket
+
+"""
+
+
+fruits = ['apple', 'banana', 'cherry']
+for index, fruit in enumerate(fruits):
+    print(index, fruit)
+
+'''
+0 apple
+1 banana
+2 cherry
+
+'''
+
+
+text = "hello"
+for index, char in enumerate(text):
+    print(index, char)
+
+'''
+0 h
+1 e
+2 l
+3 l
+4 o
+
+'''
+
+
+animals = ('cat', 'dog', 'rabbit')
+for index, animal in enumerate(animals):
+    print(index, animal)
+
+'''
+0 cat
+1 dog
+2 rabbit
+
+'''
